6c9122b51a33312206d9cf7ef5b26498e9b1c394
aa920502 <davidlucs@gmail.com>
2015-10-24 19:06:53 -0400

    delete

--- Twitter_Map/sentiment.js
+++
@@ -1,8 +0,0 @@
-
-
-    var sentiment = require('sentiment');
-
-
-    sentiment('what is it that you are looking for is not good', function(err, result) {
-        console.log('Result is %j', result);
-    });
